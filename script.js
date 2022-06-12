344dcd7bea5086df80fe93aa8314c4c4cbce027a
bambosan <bambosan888@gmail.com>
2022-06-12 08:30:23 +0700

    several updates

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
 
-window.onload =()=> {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload =()=> getxmldat(xhttp);
-    xhttp.open("GET", "https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/DigitalForecast-Indonesia.xml");
-    xhttp.send();
-}
 
-getxmldat =(xml)=> {
-    let xmldat = xml.responseXML.getElementsByTagName("forecast")[0];
-    let ladata = []
-    let lodata = []
+fetch('https://cuaca-gempa-rest-api.vercel.app/weather/jawa-barat').then((r)=> { return r.json() }).then((obj)=> { 
+    
+    console.log(obj.data) 
 
-    for(let i = 3; i < xmldat.childNodes.length - 1; i++){
-        let la = xmldat.childNodes[i].getAttribute("latitude")
-        let lo = xmldat.childNodes[i].getAttribute("longitude")
 
-        ladata.push(la)
-        lodata.push(lo)
-    }
-}
+});
+
+console.log(json);
 
 fnearest =(arr, val)=> {
     return arr.reduce((total, num)=> Math.abs(total) > Math.abs(num - val) ? num - val : total) + val;
