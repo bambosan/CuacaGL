44b942eddc08b38bda629738ff8d997e63cb8205
bambosan <bambosan888@gmail.com>
2022-06-17 09:12:32 +0700

    several updates

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 
 
-fetch('https://cuaca-gempa-rest-api.vercel.app/weather/jawa-barat').then((r)=> { return r.json() }).then((obj)=> { 
-    
-    console.log(obj.data) 
+loadjson = async(url) => {
+    return (await (await fetch(url)).json());
+}
 
-
-});
-
-console.log(json);
-
-fnearest =(arr, val)=> {
+fnearest = (arr, val) => {
     return arr.reduce((total, num)=> Math.abs(total) > Math.abs(num - val) ? num - val : total) + val;
 }
+
+var locnam =[]
+loadjson('https://cuaca-gempa-rest-api.vercel.app/weather/indonesia').then((obj) => {
+    let la = [];
+    let lo = [];
+    let numloc = obj.data.areas.length;
+    for(let i = 0; i < numloc; i++){
+        la.push(obj.data.areas[i].latitude);
+        lo.push(obj.data.areas[i].longitude);
+    }
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((pos) => {
+            let nla = fnearest(la, pos.coords.latitude);
+            let nlo = fnearest(lo, pos.coords.longitude);
+            let locnum = 0;
+            for(let i = 0; i < numloc; i++){
+                if(la[i] == nla || lo[i] == nlo) break;
+                locnum++;
+            }
+
+        });
+    } else console.log("geoloaction not supported by browser");
+});
+console.log(locnam);
 
