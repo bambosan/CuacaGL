055e68043310abbbe482cfc381b7bee55206df8d
bambosan <bambosan888@gmail.com>
2022-06-28 17:22:22 +0700

    update index.html

--- script.js
+++ script.js
@@ -1,170 +1,76 @@
 
 
-const load = async(url) => {
-    return (await (await fetch(url)).json());
-}
+const dom = ['Aceh', 'Bali', 'Banten', 'Bengkulu', 'DI Yogyakarta', 'DKI Jakarta', 'Gorontalo', 'Jambi', 'Jawa Barat', 'Jawa Tengah', 'Jawa Timur', 'Kalimantan Barat', 'Kalimantan Selatan', 'Kalimantan Tengah', 'Kalimantan Timur', 'Kalimantan Utara', 'Kep. Bangka Belitung', 'Kep. Riau', 'Lampung', 'Maluku', 'Maluku Utara', 'Nusa Tenggara Barat', 'Nusa Tenggara Timur', 'Papua', 'Papua Barat', 'Riau', 'Sulawesi Barat', 'Sulawesi Selatan', 'Sulawesi Tenggara', 'Sulawesi Utara', 'Sumatera Barat', 'Sumatera Selatan', 'Sumatera Utara'];
 
-fnear = (arr, val) => {
-    return arr.reduce((total, num) => Math.abs(total) > Math.abs(num - val) ? num - val : total) + val;
-}
+const desc = ['Banda Aceh', 'Denpasar', 'Serang', 'Bengkulu', 'Yogyakarta', 'Jakarta Pusat', 'Gorontalo', 'Jambi', 'Bandung', 'Semarang', 'Surabaya', 'Pontianak', 'Banjarmasin', 'Palangkaraya', 'Samarinda', 'Tarakan', 'Pangkal Pinang', 'Tanjung Pinang', 'Bandar Lampung', 'Ambon', 'Ternate', 'Mataram', 'Kupang', 'Kota Jayapura', 'Manokwari', 'Pekanbaru', 'Mamuju', 'Makassar', 'Kendari', 'Manado', 'Padang', 'Palembang', 'Medan'];
 
-const shader = new PetitGL(undefined, [0,0,0,1])
-	.resize(innerWidth, innerHeight)
-	.att([ {name:'_uv',data:[-1,-1, 1,-1, -1,1, 1,1],slice:2} ])
-	.ibo([ {name:'ibo',data:[0,1,2, 3,2,1]} ])
-	.compile(
-		'weatherdisp',//program name
-    
-		`attribute vec2 UV;
-		void main(){
-			gl_Position=vec4(UV,0,1);
-		}`,//vsh
+const lat = ['5.54491', '-8.643480', '-6.133333334', '-3.794373', '-7.80279', '-6.176396', '0.53175', '-1.61', '-6.90992', '-6.959144', '-7.27981', '-0.08902', '-3.32828', '-2.21', '-0.43018', '3.329128', '-2.19216', '0.906334', '-5.42396', '-3.67265', '0.833215', '-8.58142', '-10.22498', '-2.556219', '-0.900000002', '0.55742', '-2.683333335', '-5.121379', '-3.966', '1.50213', '-0.88', '-2.978699', '3.66681'];
 
-		`precision highp float;
-        //grass : https://www.shadertoy.com/view/lslGR8
-		uniform float time;
-        uniform float ws;
-		uniform vec2 res;
-	
-        #define BLADES 30
+const lon = ['95.34312', '115.224609', '106.1667', '102.304688', '110.37625', '106.826591', '123.08807', '103.59', '107.64691', '110.438690', '112.71109', '109.35144', '114.57315', '113.92', '117.17432', '117.571049', '106.12806', '104.562378', '105.25113', '128.23381', '127.365532', '116.11737', '123.60374', '140.703278', '134.00001', '101.46218', '118.8333', '119.418983', '122.6', '124.85236', '100.4', '104.751892', '98.67123'];
 
-        vec3 rotateY(float a, vec3 v){
-	        return vec3(cos(a) * v.x + sin(a) * v.z, v.y, cos(a) * v.z - sin(a) * v.x);
-        }
+const shader = new PetitGL(undefined, [0,0,0,1]).resize(innerWidth, innerHeight).att([ {name:'_uv',data:[-1,-1, 1,-1, -1,1, 1,1], slice:2} ]).ibo([ {name:'ibo',data:[0,1,2, 3,2,1]} ]).compile(
+'weatherdisp',//program name
+`attribute vec2 UV;
+void main(){
+    gl_Position=vec4(UV,0,1);
+}`,//vsh
+`precision highp float;
+uniform float time;
+uniform float ws; // wind speed
+uniform vec2 res;
+void main(){
+	gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}`//fsh
+).defAtt('weatherdisp', ['UV']).defUni('weatherdisp', ['time', 'ws', 'res']);
 
-        vec4 grass(vec2 p, float x){
-            float s = mix(0.7, 2.0, 0.5 + sin(x * 12.0) * 0.5);
-            p.x += pow(1.0 + p.y, 2.0) * 0.1 * cos(x * 0.5 + time * floor(ws));
-            p.x *= s;
-            p.y = (1.0 + p.y) * s - 1.0;
-            float m = 1.0 - smoothstep(0.0, clamp(1.0 - p.y * 1.5, 0.01, 0.6) * 0.2 * s, pow(abs(p.x) * 19.0, 1.5) + p.y - 0.6);
-            return vec4(mix(vec3(0.05, 0.1, 0.0) * 0.8, vec3(0.0, 0.3, 0.0), (p.y + 1.0) * 0.5 + abs(p.x)), m * smoothstep(-1.0, -0.9, p.y));
-        }
+fnear = (arr, val) => { return arr.reduce((total, num) => Math.abs(total) > Math.abs(num - val) ? num - val : total) + val; }
 
-        float rand(vec2 p){
-            return fract(sin(dot(p.xy ,vec2(54.90898,18.233))) * 4337.5453);
-        }
-        vec3 backg(vec2 uv){
-            float cost = clamp(dot(vec3(0.0, 0.1, 0.0), normalize(vec3(uv, -1.0))), 0.0, 1.0);
-            float sun = 1.0 - distance(uv, vec2(0.5, 0.5));
-                sun = clamp(sun,0.0,1.0);
-    
-            float glow = sun;
-            glow = clamp(glow,0.0,1.0);
-    
-            sun = pow(sun,100.0);
-            sun *= 100.0;
-            sun = clamp(sun,0.0,1.0);
-            glow = pow(glow,6.0) * 1.0;
-            glow = clamp(glow,0.0,1.0);
-            sun += glow * 0.5;
-
-            float sstars = step(rand(floor((uv * 100.0) * mat2(cos(time*0.01), -sin(time*0.01), sin(time*0.01), cos(time*0.01)))), 0.01) * 0.3;
-            return mix(vec3(0.2, 0.3, 0.4)* 0.7, vec3(0.0, 0.0, 0.3),max(0.0, uv.y + 1.)) + sstars + sun;
-        }
-
-		void main(){
-	        vec3 cw = normalize(vec3(0, 0, -1));
-	        vec3 cu = normalize(cross(cw, vec3(0, 1, 0)));
-	        vec3 cv = normalize(cross(cu, cw));
-
-	        vec2 uv = (gl_FragCoord.xy / res.xy) * 2.0 - 1.0;
-	        uv.x *= res.x / res.y;
-	        vec3 ro = vec3(0, 2, 0), rd = mat3(cu, cv, cw) * vec3(uv, -1.);
-	
-	        vec3 fcol = backg(uv);
-	
-	        for(int i = 0; i < BLADES; i += 1){
-		        float z = -(float(BLADES) * 0.05 + 1.0);
-		        float t = (z - dot(vec3(0,0,-1), ro)) / dot(vec3(0,0,-1), rd);
-		        vec2 tc = ro.xy + rd.xy * t;
-		        tc.x += cos(float(i) * 3.0) * 5.0;
-		
-		        float cell = floor(tc.x);
-		        tc.x = (tc.x - cell) - 0.5;
-		
-		        vec4 c = grass(tc, float(i) + cell * 10.0);
-		        fcol = mix(fcol, c.rgb, step(0.0, t) * c.w);
-	        }
-	
-	        fcol = mix(vec3(length(fcol)), fcol, 0.5) * 0.4;
-
-	        gl_FragColor = vec4(fcol * 1.8, 1.0);
-		}`//fsh
-	)
-	.defAtt('weatherdisp', ['UV'])
-	.defUni('weatherdisp', ['time', 'ws', 'res']);
-
-load('https://cuaca-gempa-rest-api.vercel.app/weather/indonesia').then(obj => {
-    let la = [];
-    let lo = [];
-    for(let i = 0; i < obj.data.areas.length; i++){
-        la.push(obj.data.areas[i].latitude);
-        lo.push(obj.data.areas[i].longitude);
-    }
-    
+main = async() => {
+    let obj = await (await fetch('https://cuaca-gempa-rest-api.vercel.app/weather/indonesia')).json();
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((pos) => {
-            let nla = fnear(la, pos.coords.latitude);
-            let nlo = fnear(lo, pos.coords.longitude);
-            let locp = 0;
+            let nla = fnear(lat, pos.coords.latitude);
+            let nlo = fnear(lon, pos.coords.longitude), locp = 0;
             for(let i = 0; i < obj.data.areas.length; i++){
-                if(la[i] == nla || lo[i] == nlo) break;
+                if(lat[i] == nla || lon[i] == nlo) break;
                 locp++;
             }
 
-            let dval = { val: [], desc: [] };
+            let dval = [], ddesc = [];
             obj.data.areas[locp].params.forEach((d, i) => {
                 let ho = new Date().getHours();
                 switch(i){
-                    case 1: dval.val.push(d.times[0].value); break;
-                    case 2: dval.val.push(d.times[0].celcius); break;
-                    case 3: dval.val.push(d.times[0].value); break;
-                    case 4: dval.val.push(d.times[0].celcius); break;
-                    case 5: dval.val.push(d.times[ho%4].celcius); break;
-                    case 6: dval.val.push(d.times[ho%4].name); break;
-                    case 7: dval.val.push(d.times[ho%4].card); break;
-                    case 8: dval.val.push(d.times[ho%4].kph); break;
-                    default: dval.val.push(d.times[ho%4].value);
+                    case 1: dval.push(d.times[0].value); break;
+                    case 2: dval.push(d.times[0].celcius); break;
+                    case 3: dval.push(d.times[0].value); break;
+                    case 4: dval.push(d.times[0].celcius); break;
+                    case 5: dval.push(d.times[ho%4].celcius); break;
+                    case 6: dval.push(d.times[ho%4].name); break;
+                    case 7: dval.push(d.times[ho%4].card); break;
+                    case 8: dval.push(d.times[ho%4].kph); break;
+                    default: dval.push(d.times[ho%4].value);
                 }
-                dval.desc.push(d.description);
+                ddesc.push(d.description);
             });
-            console.log(Math.floor(dval.val[8]));
+            
             const loop = () => {
                 requestAnimationFrame(loop);
                 shader.uni('weatherdisp',
                     [
                         {loc:'time', data:[(Date.now()*.001)%86400-43200], type:'f'},
-                        {loc:'ws', data:[dval.val[8]], type: 'f'},
+                        {loc:'ws', data:[dval[8]], type: 'f'},
                         {loc:'res', data:[shader.c.width, shader.c.height], type:'f'},
                     ]
                 ).draw('weatherdisp',[{loc:'UV',att:'_uv'}],'ibo').flush()
             };
             loop();
-            
+
             document.body.insertAdjacentHTML("beforeend", `
 <div style="position: fixed; width: 100vw;">
-<pre style="padding: 10px; font-size: 1.2em; color: white">
-Lokasi : ${obj.data.areas[locp].domain}
-${dval.desc[6]} : ${dval.val[6]}
-
-${dval.desc[0]} : ${dval.val[0]}
-${dval.desc[3]} : ${dval.val[3]}
-${dval.desc[1]} : ${dval.val[1]}
-
-${dval.desc[5]} : ${dval.val[5]}
-${dval.desc[4]} : ${dval.val[4]}
-${dval.desc[2]} : ${dval.val[2]}
-
-${dval.desc[7]} : ${dval.val[7]}
-${dval.desc[8]} : ${dval.val[8]} km/h
-
-this project still wip..
-PetitGL library by <a href="https://mcbeeringi.github.io/petitgl/">McbeEringi</a>
-</pre> </div>`)
+<p>${ddesc}</p>
+</div>`)
             document.body.appendChild(shader.c);
-            //console.log(dval);
         });
     } else console.log("geoloaction not supported by browser");
-});
-
+}
+main();
